310a461006fb2ebf6d937cda0b6c90ac1aef6682
Imravirathore <ravirathore672@gmail.com>
2023-06-22 18:38:59 +0530

    Uploading Project on AWS

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+import { useLocation, NavLink } from "react-router-dom";
+
+import { Nav } from "react-bootstrap";
+
+import logo from "../../assets/img/profile_img.jpg";
+import site from "../../assets/img/profileGenerator2.png";
+
+
+
+// Getting Loggedin user Name and Email
+
+const userName = JSON.parse(localStorage.getItem('userName'));
+
+function Sidebar({ color, image, routes }) {
+  const location = useLocation();
+  const activeRoute = (routeName) => {
+    return location.pathname.indexOf(routeName) > -1 ? "active" : "";
+  };
+  return (
+    <div className="sidebar">
+      <div
+        className="sidebar-background"
+        style={{
+          backgroundImage: "url(" + image + ")",
+        }}
+      />
+      <div className="sidebar-wrapper" style={{ backgroundColor: "#1b1b1b" }}>
+        <div className="logo d-flex align-items-center justify-content-start" style={{height:'100px'}}>
+          <a
+            href=""
+            className="simple-text logo-mini mx-1"
+          >
+            <div className="logo-img">
+              <img src={site} width='150' height='150' alt="..." />
+            </div>
+          </a>
+         
+        </div>
+        <Nav>
+          {routes.map((prop, key) => {
+            if (!prop.redirect)
+              return (
+                <li
+                  className={
+                    prop.upgrade
+                      ? "active active-pro"
+                      : activeRoute(prop.layout + prop.path)
+                  }
+                  key={key}
+                >
+                  <NavLink
+                    to={prop.layout + prop.path}
+                    className="nav-link"
+                    activeClassName="active"
+                  >
+                    <i className={prop.icon} />
+                    <p>{prop.name}</p>
+                  </NavLink>
+                </li>
+              );
+            return null;
+          })}
+        </Nav>
+
+        {/* Logout */}
+
+        <div
+          style={{
+            backgroundColor: "",
+            width: "260px",
+            position: "fixed",
+            bottom: "50px",
+            padding:'0 10px'
+          }}
+        >
+
+        <div style={{
+          display: "flex",
+          justifyContent: "flex-start",
+          backgroundColor:''
+      
+        }}>
+        <div className="login_img">
+        <img
+          src={logo}
+          alt=""
+          srcset=""
+          style={{ width: "40px", borderRadius: "50px", backgroundColor:'' }}
+        />
+      </div>
+
+      <div className="profile_content" style={{backgroundColor:'',marginLeft:'10px'}}>
+        <h6>{(userName && userName.fname) ? userName.fname : 'User Name'}</h6>
+        <p>{(userName && userName.email) ? userName.email : 'Email'}</p>
+      </div>
+        </div>
+       
+   
+      
+          
+        </div>
+
+        
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/views/Dashboard.js
@@ -0,0 +1,32 @@
+
+import React, { useState, useRef, useEffect } from "react";
+// react-bootstrap components
+import {
+  Badge,
+  Button,
+  Card,
+  Form,
+  Navbar,
+  Nav,
+  Container,
+  Row,
+  Col
+} from "react-bootstrap";
+
+function Dashboard() {
+
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <Col>
+         Dashboard Page
+        
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default Dashboard;
